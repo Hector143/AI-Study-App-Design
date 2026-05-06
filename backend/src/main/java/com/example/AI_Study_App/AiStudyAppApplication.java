4f0e12897389ca4b72b930d7c69000b168ab53ac
cvrsed22 <virchettautoan@gmail.com>
2026-05-06 11:46:30 +0800

    pringboot added

---
+++ backend/src/main/java/com/example/AI_Study_App/AiStudyAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.AI_Study_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiStudyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiStudyAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/AI_Study_App/AiStudyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AI_Study_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiStudyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
